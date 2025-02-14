bd2c7637827217d03c89b12bbbc4f1cd7caa7fa8
mtaut <146488331+mtaut@users.noreply.github.com>
2025-02-14 13:43:27 -0500

    update to settings

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['safe-beyond-32308.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['safe-beyond-32308-d47dd6a17261.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
